19e6390692877313fe6de983496aa3677a21c767
Benjamin Corbett <111916951+bcsurf2822@users.noreply.github.com>
2025-03-23 20:38:32 +0000

    finsih 16 Built-IN Val Attributes

--- Controllers/Models/StudentDTO.cs
+++ Controllers/Models/StudentDTO.cs
@@ -1,18 +1,30 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace CollegeApp.Models
 {
   public class StudentDTO
   {
+    [ValidateNever] //Field will not be validated
     public int Id { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Student Name is Required")]
+    [StringLength(30)]
     public string StudentName { get; set; }
 
-    [EmailAddress]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string Email { get; set; }
+
+    [Range(10, 20)]
+    public int Age { get; set; }
 
     [Required]
     public string Address { get; set; }
+
+    public string Password { get; set; }
+    // [Compare("Password")] //Confirm p.word will be compare with password field / OR
+    [Compare(nameof(Password))]
+
+    public string ConfirmPassword { get; set; }
   }
 }
